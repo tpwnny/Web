21143daee6a549d934e2976f164d5666f6663f87
tpwnny <132621253+tpwnny@users.noreply.github.com>
2023-08-25 20:02:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,141 +1,62 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
-# HTML template
-html_template = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Audio Processing App</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background-color: #f5f5f5;
-        }
-        h1 {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        form {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-        input[type="file"] {
-            display: none;
-        }
-        #upload_button {
-            background-color: #007bff;
-            color: #ffffff;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
-    </style>
-    <script>
-  
-        document.addEventListener("DOMContentLoaded", function () {
-            const fileInput = document.querySelector("#audio_file");
-            const uploadButton = document.querySelector("#upload_button");
-            
-            uploadButton.addEventListener("click", function () {
-                fileInput.click();
-            });
-            
-            fileInput.addEventListener("change", function () {
-                const fileName = fileInput.value.split("\\").pop();
-                uploadButton.innerHTML = fileName ? fileName : "Choose a file
-        document.addEventListener("DOMContentLoaded", function () {
-            const fileInput = document.querySelector("#audio_file");
-            const uploadButton = document.querySelector("#upload_button");
-            const submitButton = document.querySelector("#submit_button");
-
-            uploadButton.addEventListener("click", function () {
-                fileInput.click();
-            });
-
-            fileInput.addEventListener("change", function () {
-                const fileName = fileInput.value.split("\\").pop();
-                uploadButton.innerHTML = fileName ? fileName : "Choose a file";
-            });
-
-            uploadButton.addEventListener("touchstart", function (event) {
-                event.preventDefault();
-                fileInput.click();
-            });
-
-            submitButton.addEventListener("click", function () {
-                if (fileInput.files.length === 0) {
-                    alert("Please choose a file before submitting.");
-                } else {
-                    // Disable submit button to prevent multiple submissions
-                    submitButton.disabled = true;
-                    submitButton.innerHTML = "Uploading...";
-                    
-                    // Create FormData object to send the file
-                    const formData = new FormData();
-                    formData.append("audio_file", fileInput.files[0]);
-                    
-                    // Send the file using Fetch API or XMLHttpRequest
-                    fetch("{{ url_for('upload') }}", {
-                        method: "POST",
-                        body: formData
-                    })
-                    .then(response => response.text())
-                    .then(result => {
-                        alert(result);
-                        submitButton.disabled = false;
-                        submitButton.innerHTML = "Upload";
-                    })
-                    .catch(error => {
-                        alert("An error occurred: " + error.message);
-                        submitButton.disabled = false;
-                        submitButton.innerHTML = "Upload";
-                    });
-                }
-            });
-        });
-    </script>
-</head>
-<body>
-    <h1>Audio Processing App</h1>
-    <form action="{{ url_for('upload') }}" method="post" enctype="multipart/form-data">
-        <input type="file" id="audio_file" name="audio_file" accept=".wav, .mp3">
-        <button type="button" id="upload_button">Choose a file</button>
-        <button type="button" id="submit_button">Upload</button>
-    </form>
-</body>
-</html>
-"""
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return html_template
+    if request.method == 'POST':
+        if 'file' in request.files:
+            file = request.files['file']
+            if file.filename == '':
+                return jsonify({'error': 'No selected file'})
+            
+            # Save the file or perform processing
+            upload_path = os.path.join('uploads', file.filename)
+            file.save(upload_path)
+            
+            return jsonify({'message': 'File uploaded and saved successfully'})
+        return jsonify({'error': 'No file uploaded'})
+    
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Audio Processing App with Uppy</title>
+        <!-- Include Uppy stylesheets -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uppy@2.2.2/dist/uppy.min.css">
+    </head>
+    <body>
+        <h1>Audio Processing App</h1>
+        <form action="/" method="post" enctype="multipart/form-data">
+            <div id="uppy-container"></div>
+            <button type="submit">Upload</button>
+        </form>
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'audio_file' not in request.files:
-        return "No file part"
-    
-    file = request.files['audio_file']
-    
-    if file.filename == '':
-        return "No selected file"
-    
-    # Process the uploaded file, e.g., save it, perform actions, etc.
-    # Add your processing logic here
-    
-    return "File uploaded and processed successfully!"
+        <!-- Include Uppy script -->
+        <script src="https://cdn.jsdelivr.net/npm/uppy@2.2.2/dist/uppy.min.js"></script>
+        <script>
+            document.addEventListener("DOMContentLoaded", function () {
+                const uppy = Uppy.Core({
+                    debug: true,
+                    autoProceed: false,
+                });
+
+                // Add Dashboard plugin
+                uppy.use(Uppy.Dashboard, {
+                    target: '#uppy-container',
+                    inline: true,
+                    width: '100%',
+                    height: 300,
+                    proudlyDisplayPoweredByUppy: false,
+                });
+            });
+        </script>
+    </body>
+    </html>
+    """
 
 if __name__ == '__main__':
+    os.makedirs('uploads', exist_ok=True)  # Create uploads directory if not exist
     app.run(debug=True)
     
